30a10f7315ceb28c001f43597d1cfaba692a766c
Liubomur Penkov <lubchikpenkov@gmail.com>
2017-09-08 21:08:31 +0300

    add ALP layout + some styles

---
+++ src/routes/Category/data.js
@@ -0,0 +1,10 @@
+const animals = [
+    {url: "http://cdn77.sadanduseless.com/wp-content/uploads/2017/02/album12.jpg"},
+    {url: "http://cdn77.sadanduseless.com/wp-content/uploads/2017/02/album12.jpg"},
+    {url: "http://cdn77.sadanduseless.com/wp-content/uploads/2017/02/album12.jpg"},
+    {url: "http://cdn77.sadanduseless.com/wp-content/uploads/2017/02/album12.jpg"},
+    {url: "http://cdn77.sadanduseless.com/wp-content/uploads/2017/02/album12.jpg"},
+    {url: "http://cdn77.sadanduseless.com/wp-content/uploads/2017/02/album12.jpg"}
+];
+
+export default animals
