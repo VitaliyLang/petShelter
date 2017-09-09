aa38bb1909f96be51531334bf778b3fecc05a462
Vitaliy P <palyushok.vitaliy@gmail.com>
2017-09-09 18:16:45 +0300

    added css for landing page in progress..

--- src/routes/LandingPage/components/LandingPage.js
+++ src/routes/LandingPage/components/LandingPage.js
@@ -2,20 +2,44 @@ import React from 'react'
 import {Link} from 'react-router'
 import PropTypes from 'prop-types'
 import './LandingPage.scss'
-import giveDog from '../assets/giveDog.jpg'
-import takeDog from '../assets/takeDog.jpg'
+import goesHome from '../assets/goes_home.jpg'
+import goesShelter from '../assets/goes_to_shelter.jpg'
 
 export const LandingPage = () => (
-    <div className = "container">
-            <Link to='/' activeClassName='page-layout__nav-item--active'>
-                <img src = {takeDog} alt="TakeDog" className="takeDog"/>
-            </Link>
 
-            <Link to='/categories' activeClassName='page-layout__nav-item--active'>
-                <img src = {giveDog} alt="GiveDog" className="giveDog"/>
-            </Link>
-    </div>
+
+    <div className = "container">
+    				
+    				<h2>Welcome to Pet Shelter</h2>
+    				<h3>Home for homeless , Place of new friendships</h3>
+    				
+
+    				<div className="relocation_nav">
+	          	<div className="goesShelter_box">
+	            <Link  className="wrap" to='/' activeClassName='page-layout__nav-item--active'>
+	            <div className='goesShelter' > 
+			        		<h1 className='homeP' >goesShelter</h1>
+				 				</div>
+	 						</Link>           
+	 						</div>
+							
+							<div className="goes_home_box">		    				
+		          <Link className="wrap" to='/categories' activeClassName='page-layout__nav-item--active'>
+		          	<div className='goesHome'>
+		          			<h1 className='homeP'>goesHome</h1>
+		          	</div>
+		          	
+			        </Link>
+			        </div>
+
+			      </div>     
+   
+  </div>
 )
 
 
 export default LandingPage
+
+
+// <img src = {goesShelter} alt="GiveDog" className="takeDog"/>
+// <img src = {goesHome} alt="TakeDog" className="goesHome"/>
