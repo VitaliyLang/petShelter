e0e47bce9ce9a60eea58aff6bddb7085dd94537b
rommandz <rmandzyuk94@gmail.com>
2017-09-11 00:12:54 +0300

    some fix whith routing, and also header

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 // We only need to import the modules necessary for initial render
 import LandingPage from './LandingPage'
 import React from 'react'
-import { Route } from 'react-router'
+import {Route} from 'react-router'
 import Category from './Category'
 import Categories from './Categories'
 import Admin from './Admin'
@@ -12,53 +12,44 @@ import Partners from './Partners'
 import StaticLayout from '../layouts/index'
 import AnimalDetails from './AnimalDetails/index'
 
-
 /*  Note: Instead of using JSX, we recommend using react-router
     PlainRoute objects to build route definitions.   */
 
 export const createRoutes = (store) => ([
-  {
-    path: '/',
-    component: LandingPage
-  },
-  {
-    path      : '/admin/:action',
-    component : Admin
-  },
-  {
-    path      : '/goes-shelter',
-    component : GoesShelter 
-  }
+    {
+        path: '/',
+        component: LandingPage
+    }, {
+        component: StaticLayout,
+        childRoutes: [
+            {
+                path: '/categories',
+                component: Categories
+            }, {
+                path: 'categories/category/:categID',
+                component: Category
+            }, {
+                path: 'animals/animal/:animalID',
+                component: AnimalDetails
+            }, {
+                path: '/admin/:action',
+                component: Admin
+            }, {
+                path: '/goes-shelter',
+                component: GoesShelter
+            }, {
+                path: '/contacts',
+                component: Contacts
+            }, {
+                path: '/about',
+                component: About
+            }, {
+                path: '/partners',
+                component: Partners
+            }
 
-]);
-
-    component: StaticLayout,
-    childRoutes: [
-      {
-        path: '/categories',
-        component: Categories
-      }, {
-        path: 'categories/category/:categID',
-        component: Category
-      }, {
-        path: 'animals/animal/:animalID',
-        component: AnimalDetails
-      }, {
-        path: '/admin/:action',
-        component: Admin
-      }, {
-        path: '/contacts',
-        component: Contacts
-      }, {
-        path: '/about',
-        component: About
-      }, {
-        path: '/partners',
-        component: Partners
-      }
-
-    ]
-  }
+        ]
+    }
 ])
 
 /*  Note: childRoutes can be chunked or otherwise loaded programmatically
