b35c7e571e6221d82dc2583ebe90309f8f7964be
Liubomur Penkov <lubchikpenkov@gmail.com>
2017-09-13 22:57:16 +0300

    lazy load

--- src/routes/Category/components/Main.js
+++ src/routes/Category/components/Main.js
@@ -1,35 +1,76 @@
 import React, { Component } from 'react'
-import AnimalItem from './AnimalItem'
 import './Main.scss'
-import animals from '../data'
+import * as animals from '../data.json'
+import { List } from 'react-virtualized'
+import { Link } from 'react-router'
 
 
-
+const listModify = [];
+for (let i = 0, y = 0; i < animals.length; i+= 3, y++) {
+    listModify[y] = [animals[i], animals[i + 1], animals[i + 2]]
+}
+function rowRenderer({
+    key,
+    index,
+    isScrolling,
+    isVisible,
+    style
+  }) {
+    const content = isScrolling
+        ? <div className='flex-container_img'>
+            <Link className = "load"><img src="http://www.myiconfinder.com/uploads/iconsets/256-256-7a1b32bc6cee8e58f09af677cbfb0255.png" /></Link>
+            <Link className = "load"><img src="http://www.myiconfinder.com/uploads/iconsets/256-256-7a1b32bc6cee8e58f09af677cbfb0255.png" /></Link>
+            <Link className = "load"><img src="http://www.myiconfinder.com/uploads/iconsets/256-256-7a1b32bc6cee8e58f09af677cbfb0255.png" /></Link>
+        </div>
+        : <div className='flex-container_img'>
+            <Link><img src={listModify[index][0].url} /></Link>
+            <Link><img src={listModify[index][1].url} /></Link>
+            <Link><img src={listModify[index][2].url} /></Link>
+        </div>
+        style.top = style.height*index;
+    return (
+        <div 
+            key={key}
+            style={style}
+        >
+            {content}
+        </div>
+    )
+}
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            scrolltop: 0
+            height : 250
         }
-        this.updateViewport = this.updateViewport.bind(this);
+        this.update = this.update.bind(this);
     }
-    componentDidMount() {
-        this.updateViewport();
-    }
-    updateViewport() {
+    update(){
         this.setState({
-            scrolltop: this.main.scrollTop
-        });
+            height: window.innerWidth * 0.8 * 0.3 / 1.33
+        })
     }
+    componentDidMount(){
+        window.addEventListener('resize', this.update);
+        this.update();
+    }
+    componentWillUnmount(){
+        window.removeEventListener('resize', this.update);
+    }
+
     render() {
         return (
-            <main className='main_category' onScroll = {this.updateViewport} ref={(main)=>this.main = main}>
-                <div className="category_container">
-                    {animals.map((animal, index) => 
-                        <AnimalItem url={animal.url} key={index} scrolltop ={this.state.scrolltop}/>
-                    )}
-                </div>
-            </main>
+            <List
+                width={1200}
+                height={600}
+                rowCount={listModify.length}
+                rowHeight={this.state.height}
+                rowRenderer={rowRenderer}
+                style={{
+                    height: '100%',
+                    width: '80%'
+                }}
+            />
         )
     }
 }
