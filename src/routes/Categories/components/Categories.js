15e952aa7ce3d1777fb39a424ec4f4035064e749
petro <vikep007@gmail.com>
2017-09-14 13:04:08 +0300

    Changing for reduce/store files

--- src/routes/Categories/components/Categories.js
+++ src/routes/Categories/components/Categories.js
@@ -1,18 +1,38 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { firebaseConnect, helpers } from 'react-redux-firebase'
 import CategoriesPanel from './CategoriesPanel'
+import PropTypes from 'prop-types'
 import './styles.scss'
 
-let categoriesArr = ['dogs','cats','elephants'];
+const { isLoaded, isEmpty, pathToJS, dataToJS } = helpers
+
+
+import {ReadItem} from '../../../components/fireBase'
+
 
 class Categories extends React.Component {
+    static propTypes = {
+        categories: PropTypes.object,
+        firebase: PropTypes.object
+    }
     render(){
+        let categoriesArr = [];
+        const { categories } = this.props;
+        categoriesArr = ReadItem(categories);
         return(
             <CategoriesPanel categories={categoriesArr}/>
         )
     }
 }
 
-export default Categories
+const wrappedCategories = firebaseConnect([
+    'categories'
+  ])(Categories)
 
-
-
+  export default connect(
+    ({firebase}) => ({
+      categories: dataToJS(firebase, 'categories'),
+      profile: pathToJS(firebase, 'profile')
+    })
+  )(wrappedCategories)
---
+++ src/store/firebaseConfig/firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: 'AIzaSyA5BxHJuAbSUSY3YOErfNufvn5IpIvZIMw',
+    authDomain: 'study-4e29c.firebaseapp.com',
+    databaseURL: 'https://study-4e29c.firebaseio.com/',
+    //storageBucket: '<your-storage-bucket>'
+}
+  // react-redux-firebase options
+const config = {
+    userProfile: 'user', // firebase root where user profiles are stored
+    enableLogging: false, // enable/disable Firebase's database logging
+}
+
+export {firebaseConfig,config}
