09ee50a7420a3fd1090f55e228dbc37034eb00a6
Liubomur Penkov <lubchikpenkov@gmail.com>
2017-09-14 19:46:03 +0300

    s

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -2,17 +2,14 @@ import { compose,createStore as createReduxStore, applyMiddleware } from 'redux'
 import makeRootReducer from './reducers/';
 import { reduxFirebase } from 'react-redux-firebase';
 import {firebaseConfig, config} from './firebaseConfig/firebase';
-
 const createStore = (initialState = {}) => { 
-	const createStoreWithMiddleware = compose(
-		reduxFirebase(firebaseConfig, config),
-		window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    	window.__REDUX_DEVTOOLS_EXTENSION__()
-		)(createReduxStore)
-
+    const createStoreWithMiddleware = compose(
+        reduxFirebase(firebaseConfig, config),
+        )(createReduxStore)
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
   const store = createStoreWithMiddleware(makeRootReducer);    
  
   return store;
 }
-
 export default createStore;
