a5db96a6e4b79ad06e89f716d3ae5967cd26df96
rommandz <rmandzyuk94@gmail.com>
2017-09-25 01:20:15 +0300

    fixed pwa for developMode, add partners, contacts

--- src/routes/Contacts/components/Contacts.jsx
+++ src/routes/Contacts/components/Contacts.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import './Contacts.scss';
+import location from '../assets/location.svg';
+import mail from '../assets/mail.svg';
+import phone from '../assets/phone.svg';
 
 class Contacts extends React.Component{
     render() {
         return (
-            <div>
-                <h1>There would be Contacts info</h1>
+            <div className='contacts_wrapper'>
+                <div className="contacts_container">
+                    <h4>FIND US</h4>
+                    <div className="contacts_info">
+                        <h5>PET SHELTER</h5>
+
+                        <address>
+                            Visit us at:<br/>
+                            Box 564, Disneyland<br/>
+                            USA
+                        </address>
+
+                        <address>
+                            <img src={phone} alt="phone_sign"/> 999 - (999) - 999<br/>
+                            <img src={mail} alt="mail_sign"/> petshelter@gmail.com<br/>
+                        </address>
+
+                        <div className="contacts_location">
+                            <img src={location} alt="location_sign"/>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
--- src/routes/Partners/components/Partners.jsx
+++ src/routes/Partners/components/Partners.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router';
+import './Partners.scss';
+import happyDog from '../assets/happy_dog.png';
+import kitekat from '../assets/kitekat.png';
+import pedigree from '../assets/pedigree.png';
+import chappi from '../assets/Chappi.png';
+import whiskas from '../assets/whiskas.png';
 
 class Partners extends React.Component{
     render() {
         return (
-            <div>
-                <h1>There would be Partners info</h1>
+            <div className='partners_wrapper'>
+                <div className='partners_container'>
+                    <h4>Our thanks</h4>
+                    <div className="partners_brands">
+                        <Link to='/partners'> <img src={happyDog} alt="happyDog"/>  </Link>
+                        <Link to='/partners'> <img src={kitekat} alt="kitekat"/> </Link>
+                        <Link to='/partners'> <img src={pedigree} alt="pedigree"/> </Link>
+                        <Link to='/partners'> <img src={whiskas} alt="whiskas"/> </Link>
+                        <Link to='/partners'> <img src={chappi} alt="chappi"/> </Link>
+                    </div>
+                </div>
             </div>
         );
     }
