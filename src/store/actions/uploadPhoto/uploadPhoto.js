e1f2e9725f3977af98e6341651e318691feb7068
petro <vikep007@gmail.com>
2017-09-27 13:14:50 +0300

    Add uploadPhoto action

--- src/store/actions/uploadPhoto/uploadPhoto.js
+++ src/store/actions/uploadPhoto/uploadPhoto.js
@@ -16,10 +16,14 @@ export function uploadPhoto(file,category,animalUid){
             })
             .then(url => {
                 let imgUrlArr = [];
-                if(typeof url.val() == 'object' && url.val()){
-                    imgUrlArr = [...url.val(),animalPhotoUrl];
+                if(url){
+                    if(typeof url.val() == 'object' && url.val()){
+                        imgUrlArr = [...url.val(),animalPhotoUrl];
+                    }else{
+                        imgUrlArr = [url.val(),animalPhotoUrl];
+                    }
                 }else{
-                    imgUrlArr = [url.val(),animalPhotoUrl];
+                    imgUrlArr = [animalPhotoUrl];
                 }
                 return database.ref('/animals/'+category+'/'+animalUid+'/url').set(imgUrlArr)
             })
