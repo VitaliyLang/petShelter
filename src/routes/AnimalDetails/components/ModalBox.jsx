ad9ef98161944327e80bad2baf7a6aebc6002fbc
Liubomur Penkov <lubchikpenkov@gmail.com>
2017-09-27 16:14:09 +0300

    modalBox

--- src/routes/AnimalDetails/components/ModalBox.jsx
+++ src/routes/AnimalDetails/components/ModalBox.jsx
@@ -1,17 +1,33 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './ModalBox.scss';
+import {Button, Input} from 'react-materialize';
 
-export default class ModalBox extends Component{
-    render(){
-        if(!this.props.show){
+export default class ModalBox extends Component {
+    constructor(props){
+        super(props);
+        this.submit = this.submit.bind(this);
+    }
+    submit(e){
+        e.preventDefault();
+        this.props.click();
+    }
+    render() {
+        if (!this.props.show) {
             return null;
         }
-        return(
-            <div className = 'modal_box_adopt'>
-                
-                <button onClick={this.props.click}>Yes</button>
+        return (
+            <div className='modal_box_adopt'>
+                <form className='modal_form' onSubmit={this.submit}>
+                    <Input label="Name" validate = {true} required  minLength= '3' size = "10"/>
+                    <Input type="email" label="Email" validate = {true} required/>
+                    <Input type = "tel" validate = {true} label="Phone: (0XX) XXX-XX-XX" required  pattern="\([0-9]{3}\)\s[0-9]{3}-[0-9]{2}-[0-9]{2}"/>
+                    <div>
+                        <Button type = "submit" className="filter waves-effect waves-light primaryBtn"> Apply </Button>
+                        <Button type = "reset" onClick={this.props.click} className="filter waves-effect waves-light primaryBtn"> Cancel </Button>
+                    </div>
+                </form>
             </div>
-            
+
         )
     }
 }
