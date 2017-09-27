4dcc50ee95c51d90f1d33057030ec791cded1051
petro <vikep007@gmail.com>
2017-09-27 13:00:10 +0300

    Add uploadPhoto action

--- src/routes/Admin/components/screens/Dashboard/index.jsx
+++ src/routes/Admin/components/screens/Dashboard/index.jsx
@@ -1,13 +1,47 @@
 import React from 'react'
+import { connect } from 'react-redux';
+import Button from 'modules/buttons/PrimaryButton';
+import uploadPhoto from '../../../../../store/actions/uploadPhoto'
+import FileInput from 'react-file-input';
 
 class Dashboard extends React.Component {
+    constructor(props){
+        super(props)
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e){
+        let photo = e.target.files[0];
+        this.props.onUploadPhoto(photo,'dog','-KupDqW5nCMTBujGEZVh')
+    }
+
     render(){
         return(
-            // <CategoriesPanel categories={categoriesArr}/>
-            <p>There will be such information: how many messages are active, how many animals are...
-            </p>
+            <form>
+        <FileInput name="myImage"
+                   accept=".png,.gif,.jpg,.jpeg"
+                   placeholder="My Image"
+                   className="inputClass"
+                   onChange={this.handleChange} />
+      </form>
         )
     }
 }
 
-export default Dashboard
+//export default Dashboard
+let mapStateToProps = (state) => {
+    return {
+      uploadPhoto: state.uploadPhoto
+    }
+  }
+  
+  let mapDispatchToProps = (dispatch) => {
+    return {
+      onUploadPhoto: (file,category,id) => dispatch(uploadPhoto(file,category,id))
+    };
+  }
+  
+  export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(Dashboard)
--- src/store/reducers/addAnimal.js
+++ src/store/reducers/addAnimal.js
@@ -1,12 +1,14 @@
 let initialState = {
     isAdding: false,
+    animalUid: null,
+    category: null,
     error: {}
 };
 
 export default (state = initialState, action) =>{    
       switch (action.type) {
         case 'ADD_ANIMAL_OK':
-            return {...state, isAdding:true,  error:{}};
+            return {...state, isAdding:true, animalUid:action.animalUid, category:action.category, error:{}};
         case 'ADD_ANIMAL_FALSE':
             return {...state, isAdding:false, error:action.error};
         default:
