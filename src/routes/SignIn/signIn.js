fcb26ebc91dd26c955e93ac37b94e97885604f5f
Vitaliy P <palyushok.vitaliy@gmail.com>
2017-09-29 01:14:12 +0300

    btn changed

--- src/routes/SignIn/signIn.js
+++ src/routes/SignIn/signIn.js
@@ -53,7 +53,7 @@ class signIn extends React.Component{
                 <Formsy.Form  className="form" onValidSubmit={this.logIn} onValid={this.enableButton} onInvalid={this.disableButton}>		
 					<MainInput name="email" type="email" validations="isEmail" placeholder="email" validations={{minLength: 7}} validationError="This is not a valid email" required/>
 					<MainInput name="password" type="password" placeholder="Password" required/>	
-					<button  type="submit" className='prim_btn' disabled={!this.state.canSubmit}>Sign In</button>
+					<Button disabled={!this.state.canSubmit}>Sign in</Button>
 				</Formsy.Form>
             </div>
         )
