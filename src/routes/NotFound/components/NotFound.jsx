959247aa509420448dd0e2161c89105f6ed9654d
rommandz <rmandzyuk94@gmail.com>
2017-09-29 23:33:01 +0300

    responsive header&sidebar, fix 404, pwa only for prod mode

--- src/routes/NotFound/components/NotFound.jsx
+++ src/routes/NotFound/components/NotFound.jsx
@@ -1,12 +1,15 @@
 import React from "react";
-import './NotFound.scss'
-import dog from '../assets/dog404.png'
+import {Link} from 'react-router';
+import './NotFound.scss';
+import dog from '../assets/dog404.png';
 
 class NotFound extends React.Component {
     render() {
         return (
             <div className="container_NotFound">
-                <img src={dog} alt="error"/>
+                <Link to = "/">
+                    <img src={dog} alt="error"/>
+                </Link>
                 <h1>File not found</h1>
                 <p>
                     The site configured at this address does not contain the requested file.
