4344da12d8e090441a0a82d2d56ae2237975d55a
rommandz <rmandzyuk94@gmail.com>
2017-09-30 23:10:35 +0300

    webpack config for PWA (prod_mode), improving cashe strategy

--- src/pwa/pwa.js
+++ src/pwa/pwa.js
@@ -1,9 +1,3 @@
-export const registerSW = () => {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/sw.js');
-    }
-}
-
 export const addManifest = () => {
     let link = document.createElement('link');
     let head = document.getElementsByTagName('head')[0];
