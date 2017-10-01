5e540a152ab8f1921e29bd70ec2f82225c748bcb
Vitaliy P <palyushok.vitaliy@gmail.com>
2017-10-02 01:12:28 +0300

    Pet info editor in progress...

--- src/routes/Admin/components/layouts/PetsTable/components/PetRow/index.jsx
+++ src/routes/Admin/components/layouts/PetsTable/components/PetRow/index.jsx
@@ -7,7 +7,26 @@ class PetRow extends React.Component {
 				super(props);
 				
 		}
+		getRow(e) {
+			var objKey = e.target.getAttribute('data-key');
+			// console.log(e.target.getAttribute('data-key'));
+			this.findClickedObj(objKey);
+		}
 
+		findClickedObj(key) {
+		console.log(key);
+		console.log(this.props.petsArr);
+		var myArr = this.props.petsArr;
+		console.log(myArr);
+		for (var i = 0; i < myArr.length; i++){
+			if (myArr[i].key == key) {
+				// console.log( myArr[i]);
+				return this.props.returnPetToUpdate(myArr[i]);					
+			}
+		} 
+
+		
+		}
 		render(){
 				return(
 						<div className="pet_row">
@@ -16,9 +35,12 @@ class PetRow extends React.Component {
 								<div className="rowItem">{this.props.item.age}</div>
 								<div className="rowItem">{this.props.item.size}</div>
 								<div className="rowItem">{this.props.item.sex}</div>
-								<div className="rowItem ">{this.props.item.active}</div>
+								<div className="rowItem ">{String(this.props.item.active)}</div>
 								<div className="rowItem img">
 										<img className="img" src={this.props.item.url} alt=""/>
+								</div>
+								<div className="rowItem ">
+									<button onClick={this.getRow.bind(this)} data-key={this.props.item.key}>Edit</button>
 								</div>
 								
 						</div>    
