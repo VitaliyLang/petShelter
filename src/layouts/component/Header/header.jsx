b043ea87b7c383a0939f3ba7289bd51271a35660
rommandz <rmandzyuk94@gmail.com>
2017-10-02 23:39:43 +0300

    Fix header problems, add about page

--- src/layouts/component/Header/header.jsx
+++ src/layouts/component/Header/header.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Link} from 'react-router';
+import { Link } from 'react-router';
 import '../../../styles/main.scss';
 import './header.scss';
 import { SideNav, SideNavItem } from 'react-materialize';
+import Logo from './petLogo.png';
 
 class Header extends React.Component{
     render() {
         return (
-                <nav className = "main-layout-header">
-                    <div className="nav-wrapper">
-                        <Link to='/' className="brand-logo">
-                            <img src={require('./petLogo.png')} alt='logo'/>
+                <nav className = 'main-layout-header'>
+                    <div className='nav-wrapper'>
+                        <Link to='/' className='brand-logo'>
+                            <img src={Logo} alt='logo'/>
                         </Link>
-                        
+
                         <SideNav
                     	trigger={
-                            <div className="button-colapse">
-                                <i className="material-icons">menu</i>
+                            <div className='button-colapse'>
+                                <i className='material-icons'>menu</i>
                             </div>
                         }
                     	options={
@@ -25,19 +26,21 @@ class Header extends React.Component{
                              menuWidth: 250 }
                          }
                     	>
-                        	<SideNavItem href='/'>
-                                <img className="sidenav-logo" src={require('./petLogo.png')} alt='logo'/>
-                            </SideNavItem>
+                        	<li>
+                                <Link to='/'>
+                                    <img className='sidenav-logo' src={Logo} alt='logo'/>
+                                </Link>
+                            </li>
                             <SideNavItem divider />
-                        	<SideNavItem href='/contacts'>Contacts</SideNavItem>
-                            <SideNavItem href='/about'>About</SideNavItem>
-                            <SideNavItem href='/partners'>Partners</SideNavItem>
+                            <li><Link to='/contacts'>Contacts</Link></li>
+                            <li><Link to='/about'>About</Link></li>
+                            <li><Link to='/partners'>Partners</Link></li>
                         </SideNav>
 
-                    <ul className="right hide-on-med-and-down">
-                        <li><Link activeClassName="active" to='/contacts'>Contacts</Link></li>
-                        <li><Link activeClassName="active" to='/about'>About</Link></li>
-                        <li><Link activeClassName="active" to='/partners'>Partners</Link></li>
+                    <ul className='right hide-on-med-and-down'>
+                        <li><Link activeClassName='active' to='/contacts'>Contacts</Link></li>
+                        <li><Link activeClassName='active' to='/about'>About</Link></li>
+                        <li><Link activeClassName='active' to='/partners'>Partners</Link></li>
                     </ul>
                     </div>
                 </nav>
