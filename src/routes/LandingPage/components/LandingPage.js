ec62ed656c3c01909ec4da8b8a7cf5729da88cf0
Liubomur Penkov <lubchikpenkov@gmail.com>
2017-10-07 19:31:10 +0300

    main page, fixed some styles

--- src/routes/LandingPage/components/LandingPage.js
+++ src/routes/LandingPage/components/LandingPage.js
@@ -12,23 +12,15 @@ export const LandingPage = () => (
   <div className='main_wrapper'>
 
     <div className='relocation_nav'>
-      <div className='goesShelter_box'>
-        <Link className='wrap' to='/goes-shelter' activeClassName='page-layout__nav-item--active'>
-          <div className='goesShelter' >
-            <h1 className='homeP' >Goes Shelter</h1>
-          </div>
-        </Link>
-      </div>
-      <Link to='/'>
+      <Link to='/goes-shelter' activeClassName='page-layout__nav-item--active'>
+            <h1 className='homeP' >Leave pet</h1>
+      </Link>
+      <Link to='/' className = "logo">
         <img src={require('../../../layouts/component/Header/petLogo.png')} className='main_page_logo' alt='logo' />
       </Link>
-      <div className='goes_home_box'>
-        <Link className='wrap' to='/categories' activeClassName='page-layout__nav-item--active'>
-          <div className='goesHome'>
-            <h1 className='homeP'>Goes Home</h1>
-          </div>
+        <Link to='/categories' activeClassName='page-layout__nav-item--active'>
+            <h1 className='homeP'>Adopt pet</h1>
         </Link>
-      </div>
     </div>
 
     <Slider className='slider' fullscreen indicators={false} interval={3000} >
@@ -36,19 +28,18 @@ export const LandingPage = () => (
         src='http://www.ultrahdfreewallpapers.com/uploads/large/animals/cat-hd-wallpaper-0166.jpg'
         title='Pet Shelter welcome you!'
         placement='left'>
-			Home for homeless , Place of new friendships.
+
 		</Slide>
       <Slide className='slide'
         src='https://daywallpaper.files.wordpress.com/2013/08/red-german-boxer-dog-portrait.jpg'
-        title='Here you can find your best friend.'
+        title='Find your little friend.'
         placement='right'>
-			It waits you!
+
 		</Slide>
       <Slide className='slide'
         src='https://www.newhdwallpapers.in/wp-content/uploads/2015/11/Cool-Parrot-HD-Wallpaper.jpg'
         title='We can accept your pet.'
         placement='left'>
-			It will be in safe hands with us!
 		</Slide>
     </Slider>
 
@@ -56,3 +47,8 @@ export const LandingPage = () => (
 )
 
 export default LandingPage
+
+//			Home for homeless , Place of new friendships.
+//			It waits you!
+//      It will be in safe hands with us!
+
