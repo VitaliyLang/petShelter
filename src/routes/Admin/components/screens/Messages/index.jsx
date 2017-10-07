2a2e8b1e48b87a7e1dac1f281fea378b60c4da78
petro <vikep007@gmail.com>
2017-10-07 16:28:12 +0300

    add message page+mode categories page

--- src/routes/Admin/components/screens/Messages/index.jsx
+++ src/routes/Admin/components/screens/Messages/index.jsx
@@ -1,34 +1,42 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import Message from './Message/'
+import './styles.scss'
 
 class Messages extends React.Component {
     constructor(props){
         super(props)
+        this.state = {messages:this.props.messages}
+    }
+
+    componentWillReceiveProps(nextProps){
+        this.setState({messages:nextProps.messages})
     }
 
     render(){
-        let messageArr = [];
-        const { users } = this.props,
-            onlyKey = true;
-        if(users){
-            messageArr = ReadItem(users,onlyKey);
-            let tamplate = [];
-            if(messageArr.length > 0){
-                tamplate = messageArr.map((item,i) => {
-                    return(                    
+
+        if(!this.state.messages){
+            return null
+        }
+
+       let keys = Object.keys(this.props.messages), 
+           messagesArr = [], tamplate = [];
+
+       messagesArr =  keys.map((item) => this.state.messages[item]);
+        
+       tamplate = messagesArr.map((item,i) => {
+                     return(                    
                         <Message key={i} message={item}/>
                     )
                 })
-            }
-            return(
-                <section className='messages'>
-                    {tamplate}
-                </section>
-            )
-        }
-        return null        
+        return(
+            <section className='messages'>
+                  {tamplate}
+            </section>
+        )
     }
 }
 
-export default Messages
+export default connect(
+    state => ({messages:state.messages.messages})
+)(Messages)
